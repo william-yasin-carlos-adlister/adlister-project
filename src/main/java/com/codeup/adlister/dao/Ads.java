85d1989543b5875fc854743baa630ecdf9dac439
WilliamBaldridge <baldridge.william09@gmail.com>
2021-04-20 16:42:28 -0500

    stage branch test

--- src/main/java/com/codeup/adlister/dao/Ads.java
+++ src/main/java/com/codeup/adlister/dao/Ads.java
@@ -5,6 +5,8 @@ import com.codeup.adlister.models.Ad;
 import java.util.List;
 
 public interface Ads {
+
+
     // get a list of all the ads
     List<Ad> all();
     // insert a new ad and return the new ad's id
