9fe42017debf3409bb22cf73a2c488190b425996
Yasin Mogultay <yasin.mglty@gmail.com>
2021-04-22 02:10:14 +0300

    cp:navbar stop reset

---
+++ src/main/java/com/codeup/adlister/controllers/DetailsPageServlet.java
@@ -0,0 +1,16 @@
+package com.codeup.adlister.controllers;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet (name = "controllers.DetailsPageServlet", urlPatterns = "/details")
+public class DetailsPageServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        req.getRequestDispatcher("/WEB-INF/ads/details-page.jsp").forward(req, resp);
+    }
+}
---
+++ src/main/webapp/WEB-INF/ads/details-page.jsp
@@ -0,0 +1,18 @@
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <jsp:include page="/WEB-INF/partials/head.jsp">
+        <jsp:param name="title" value="Show Ad Details" />
+    </jsp:include>
+</head>
+<body>
+<jsp:include page="/WEB-INF/partials/navbar.jsp" />
+
+<div class="container">
+    <h1>Details!</h1>
+
+</div>
+
+</body>
+</html>
