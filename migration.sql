9d0f378260a04b33854ea74d55e6a603c3333039
Yasin Mogultay <yasin.mglty@gmail.com>
2021-04-21 17:44:44 +0300

    feat:fixed table

--- migration.sql
+++ migration.sql
@@ -6,33 +6,37 @@ DROP TABLE IF EXISTS users;
 DROP TABLE IF EXISTS categories;
 
 
-CREATE TABLE users (
-    id INT UNSIGNED NOT NULL AUTO_INCREMENT,
+CREATE TABLE users
+(
+    id       INT UNSIGNED NOT NULL AUTO_INCREMENT,
     username VARCHAR(240) NOT NULL,
-    email VARCHAR(240) NOT NULL,
+    email    VARCHAR(240) NOT NULL,
     password VARCHAR(255) NOT NULL,
     PRIMARY KEY (id)
 );
 
-CREATE TABLE ads (
-    id INT UNSIGNED NOT NULL AUTO_INCREMENT,
-    user_id INT UNSIGNED NOT NULL,
-    title VARCHAR(240) NOT NULL,
-    description TEXT NOT NULL,
+CREATE TABLE ads
+(
+    id          INT UNSIGNED NOT NULL AUTO_INCREMENT,
+    user_id     INT UNSIGNED NOT NULL,
+    title       VARCHAR(240) NOT NULL,
+    description TEXT         NOT NULL,
     PRIMARY KEY (id),
-    FOREIGN KEY (user_id) REFERENCES users(id)
+    FOREIGN KEY (user_id) REFERENCES users (id)
         ON DELETE CASCADE
 );
 
-CREATE TABLE categories (
-    id INT UNSIGNED NOT NULL AUTO_INCREMENT,
+CREATE TABLE categories
+(
+    id   INT UNSIGNED NOT NULL AUTO_INCREMENT,
     name VARCHAR(240) NOT NULL,
-    PRIMARY KEY(id)
+    PRIMARY KEY (id)
 );
 
-CREATE TABLE ad_category (
-    ad_id INT UNSIGNED NOT NULL,
+CREATE TABLE ad_category
+(
+    ad_id       INT UNSIGNED NOT NULL,
     category_id INT UNSIGNED NOT NULL,
-    FOREIGN KEY (ad_id) REFERENCES ads(id),
-    FOREIGN KEY (category_id) REFERENCES categories(id)
+    FOREIGN KEY (ad_id) REFERENCES ads (id),
+    FOREIGN KEY (category_id) REFERENCES categories (id)
 );
