70071e27c4a03cfb9113c22ae96ffd5213fd2f84
Yasin Mogultay <yasin.mglty@gmail.com>
2021-04-22 22:41:58 +0300

    feat:done with show ads page

--- src/main/java/com/codeup/adlister/controllers/DetailsPageServlet.java
+++ src/main/java/com/codeup/adlister/controllers/DetailsPageServlet.java
@@ -1,5 +1,8 @@
 package com.codeup.adlister.controllers;
 
+import com.codeup.adlister.dao.DaoFactory;
+import com.codeup.adlister.dao.MySQLAdsDao;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -9,8 +12,17 @@ import java.io.IOException;
 
 @WebServlet (name = "controllers.DetailsPageServlet", urlPatterns = "/details")
 public class DetailsPageServlet extends HttpServlet {
+
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        long id = Long.parseLong(req.getParameter("id"));
+        try {
+            req.setAttribute("ad", DaoFactory.getAdsDao().getById(id));
+//            req.setAttribute("user", DaoFactory.getUsersDao().getUserById(id));
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
         req.getRequestDispatcher("/WEB-INF/ads/details-page.jsp").forward(req, resp);
     }
+
 }
--- src/main/webapp/WEB-INF/ads/details-page.jsp
+++ src/main/webapp/WEB-INF/ads/details-page.jsp
@@ -3,16 +3,20 @@
 <html>
 <head>
     <jsp:include page="/WEB-INF/partials/head.jsp">
-        <jsp:param name="title" value="Show Ad Details" />
+        <jsp:param name="title" value="Ad Details"/>
     </jsp:include>
 </head>
 <body>
-<jsp:include page="/WEB-INF/partials/navbar.jsp" />
-
-<div class="container">
-    <h1>Details!</h1>
-
-</div>
-
+<jsp:include page="/WEB-INF/partials/navbar.jsp"/>
+<br>
+    <div class="card mt3 mb3 mr3 ml3 container">
+        <h5 class="card-header">Title: ${ad.title}</h5>
+        <div class="card-body">
+            <h5 class="card-title">User Id: ${ad.userId}</h5>
+            <p class="card-text">Description: ${ad.description}</p>
+<%--            <p class="card-text">Created by: ${user.username}</p>--%>
+            <a href="/ads" class="btn btn-primary">Ads</a>
+        </div>
+    </div>
 </body>
 </html>
